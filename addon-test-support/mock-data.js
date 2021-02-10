ded142a7cf64fa8fd56ae90a6f3eefd859428e33
andrew-paterson <andrew.paterson9@gmail.com>
2021-02-10 17:27:20 +0200

    Test

---
+++ addon-test-support/mock-data.js
@@ -0,0 +1,14 @@
+export default {
+  "data": {
+    "attributes": {
+      "someKey": "Some value",
+      "id": 172509777363005,
+      
+    },
+    "id": "172509777363005",
+    "type": "sanger-job-overview"
+  },
+  "jsonapi": {
+    "version": "1.0"
+  }
+}
